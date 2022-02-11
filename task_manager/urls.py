87a4cfdad5f5e78dd3882d0dd3f6123018cbec93
Naman Gogia <namangogia2001@gmail.com>
2022-02-12 03:42:03 +0530

    Add mark and view completed tasks

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,8 +15,35 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from tasks.views import (
+    all_tasks_view,
+    GenericTaskView,
+    GenericTaskCreateView,
+    GenericTaskUpdateView,
+    GenericTaskDetailView,
+    GenericTaskDeleteView,
+    session_storage_view,
+    UserCreateView,
+    UserLoginView,
+    GenericCompletedTaskView,
+    GenericMarkTaskAsCompleteView,
+    GenericAllTaskView,
+)
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("all_tasks/", GenericAllTaskView.as_view()),
+    path("tasks/", GenericTaskView.as_view()),
+    path("completed_tasks/", GenericCompletedTaskView.as_view()),
+    path("create-task/", GenericTaskCreateView.as_view()),
+    path("update-task/<pk>", GenericTaskUpdateView.as_view()),
+    path("detail-task/<pk>", GenericTaskDetailView.as_view()),
+    path("delete-task/<pk>/", GenericTaskDeleteView.as_view()),
+    path("complete_task/<pk>/", GenericMarkTaskAsCompleteView.as_view()),
+    path("user/signup", UserCreateView.as_view()),
+    path("user/login", UserLoginView.as_view()),
+    path("user/login", UserLoginView.as_view()),
+    path("user/logout", LogoutView.as_view()),
+    path("sessiontest", session_storage_view),
 ]
---
+++ tasks/views.py
@@ -0,0 +1,194 @@
+from re import template
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from tasks.models import Task
+
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.forms import ModelForm
+from django.core.exceptions import ValidationError
+from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class AuthorizedTaskManager(LoginRequiredMixin):
+    def get_queryset(self):
+        tasks = Task.objects.filter(
+            deleted=False, completed=False, user=self.request.user
+        )
+        return tasks
+
+
+################################ Pending tasks ##########################################
+class GenericTaskView(LoginRequiredMixin, ListView):
+    queryset = Task.objects.filter(deleted=False, completed=False)
+    template_name = "pending_tasks.html"
+    context_object_name = "tasks"
+    paginate_by = 5
+
+    def get_queryset(self):
+        search_term = self.request.GET.get("search")
+        tasks = Task.objects.filter(
+            deleted=False, completed=False, user=self.request.user
+        )
+
+        if search_term:
+            tasks = tasks.filter(title__icontains=search_term)
+
+        return tasks
+
+
+################################ Completed tasks ##########################################
+class GenericCompletedTaskView(LoginRequiredMixin, ListView):
+    queryset = Task.objects.filter(completed=True)
+    template_name = "completed_tasks.html"
+    context_object_name = "tasks"
+    paginate_by = 5
+
+    def get_queryset(self):
+        search_term = self.request.GET.get("search")
+        tasks = Task.objects.filter(completed=True, user=self.request.user)
+
+        if search_term:
+            tasks = tasks.filter(title__icontains=search_term)
+
+        return tasks
+
+
+################################ All tasks ##########################################
+class GenericAllTaskView(LoginRequiredMixin, ListView):
+    queryset = Task.objects.filter(deleted=False)
+    template_name = "all_tasks.html"
+    context_object_name = "tasks"
+
+    def get_context_data(self, **kwargs):
+        context = super(GenericAllTaskView, self).get_context_data(**kwargs)
+        context["active_tasks"] = Task.objects.filter(deleted=False, completed=False)
+        context["completed_tasks"] = Task.objects.filter(completed=True)
+        search_term = self.request.GET.get("search")
+        if search_term:
+            context["active_tasks"] = context["active_tasks"].filter(
+                title__icontains=search_term
+            )
+            context["completed_tasks"] = context["completed_tasks"].filter(
+                title__icontains=search_term
+            )
+        return context
+
+
+def all_tasks_view(request):
+    search_term = request.GET.get("search")
+    return render(
+        request,
+        "all_tasks.html",
+        {
+            "active_tasks": get_tasks("pending", search_term),
+            "completed_tasks": get_tasks("completed", search_term),
+        },
+    )
+
+
+################################ Task Detail View ##########################################
+class GenericTaskDetailView(AuthorizedTaskManager, DetailView):
+    model = Task
+    template_name = "task_detail.html"
+
+
+################################ Add a task ##########################################
+class TaskCreateForm(ModelForm):
+    def clean_title(self):
+        # cleaned_data is django's representation of all data collected from the form
+        title = self.cleaned_data["title"]
+        if len(title) < 10:
+            raise ValidationError("Error: Length must be 10 characters")
+        return title.upper()
+
+    class Meta:
+        model = Task
+        fields = ("title", "description", "completed")
+
+
+class GenericTaskCreateView(LoginRequiredMixin, CreateView):
+    form_class = TaskCreateForm
+    template_name = "task_create.html"
+    success_url = "/tasks"
+
+    # Overriding the following method present in the base class
+    def form_valid(self, form):
+        """If the form is valid, save the associated model."""
+        self.object = form.save()
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+
+################################ Update a task ##########################################
+class GenericTaskUpdateView(AuthorizedTaskManager, UpdateView):
+    model = Task
+    form_class = TaskCreateForm
+    template_name = "task_update.html"
+    success_url = "/tasks"
+
+
+################################ Delete a task ##########################################
+class GenericTaskDeleteView(AuthorizedTaskManager, DeleteView):
+    model = Task
+    template_name = "task_delete.html"
+    success_url = "/tasks"
+
+
+################################ Mark task as complete ##########################################
+class GenericMarkTaskAsCompleteView(AuthorizedTaskManager, UpdateView):
+    model = Task
+    fields = []
+    template_name = "task_complete.html"
+    success_url = "/tasks"
+
+    def form_valid(self, form):
+        self.object = form.save()
+        self.object.completed = True
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+
+################################ Helper function to return pending tasks and completed tasks ##########################################
+def get_tasks(task_type, search_term):
+    tasks = None
+    if task_type == "pending":
+        tasks = Task.objects.filter(deleted=False, completed=False)
+    elif task_type == "completed":
+        tasks = Task.objects.filter(completed=True)
+
+    if search_term:
+        tasks = tasks.filter(title__icontains=search_term)
+
+    return tasks
+
+
+################################ Session Storage ##########################################
+def session_storage_view(request):
+    print(
+        request.session
+    )  # <django.contrib.sessions.backends.db.SessionStore object at 0x7f8fa8126d90> (It is a dict)
+
+    # Get the total views from the session
+    total_views = request.session.get("total_views", 0)
+    # Store the value back in the session
+    request.session["total_views"] = total_views + 1
+    # Render it back to us
+    return HttpResponse(
+        f"Total views is {total_views} and the user is {request.user} and are they authenticated? {request.user.is_authenticated}"
+    )
+
+
+################################ User Sign Up ##########################################
+class UserCreateView(CreateView):
+    form_class = UserCreationForm
+    template_name = "user_create.html"
+    success_url = "/user/login"
+
+
+class UserLoginView(LoginView):
+    template_name = "user_login.html"
